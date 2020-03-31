a22a3b8293788d78e24a59c87def78794e681c8a
cantstopcoding <jonathan_koss@yahoo.com>
2020-03-31 18:00:54 -0400

    define get_genre method

--- lib/best_books_2019/cli.rb
+++ lib/best_books_2019/cli.rb
@@ -1,10 +1,15 @@
 class BestBooks2019::CLI
     def call
         puts "Welcome! Select a genre and see the best books in 2019 for it..."
+        # get_advertised_genres
         # get_user_genre
         # get_books_for(genre)
         # list books
     end
+
+    def get_genres # get_advertised_genres
+        @genres = []
+    end 
 
     def get_user_genre 
         genre = ['']
