77e76ba0610a6025c97a4fce48150869c46483a3
cantstopcoding <jonathan_koss@yahoo.com>
2020-03-31 17:38:58 -0400

    add comments in call method and made a #get_user_genre_method

--- lib/best_books_2019/cli.rb
+++ lib/best_books_2019/cli.rb
@@ -1,6 +1,12 @@
 class BestBooks2019::CLI
     def call
         puts "Welcome! Select a genre and see the best books in 2019 for it..."
-        # binding.pry
+        # get_user_genre
+        # get_books_for(genre)
+        # list books
+    end
+
+    def get_user_genre 
+        genre = ['']
     end
 end
