659c18dd5dc48f6ff93899aff287236cc9263b6e
cantstopcoding <jonathan_koss@yahoo.com>
2020-04-02 15:39:48 -0400

    scraped

--- lib/best_books_2019/scraper.rb
+++ lib/best_books_2019/scraper.rb
@@ -2,6 +2,12 @@ class BestBooks2019::Scraper
     def self.scrape_genres
         site = "https://www.goodreads.com/choiceawards/best-books-2019"
         page = Nokogiri::HTML(open(site))
-        binding.pry
+        genres = page.css("h4.category__copy")
+
+        genres.each do |genre| 
+            name = genre.text.strip
+            BestBooks2019::Genre.new(name)
+        end 
+        # binding.pry
     end
 end 
